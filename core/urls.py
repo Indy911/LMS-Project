6c4f01cce3e90513a93b19ef9d913ec4ea138191
Indy911 <indrajroy63@gmail.com>
2025-09-17 00:01:02 +0100

    Fixed Librarian form centering issue in signup-librarian.html template

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from . import views #Imports views from the current app
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup-mainpage/', views.signupmain, name='signup-mainpage'),
     path('login-mainpage/', views.loginmain, name='login-mainpage'),
-    path('signup-mainpage/', views.signupmain, name='singup-mainpage'),
     path('signup-librarian/', views.signup_librarian, name='signup-librarian'),
     path('librarian_list/', views.librarian_search, name='librarian_list'),
 ]
